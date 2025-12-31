aa37f598121fa305507782c245b18ceba0794047
Ashwani Tripathi <ashwani.tripathi@brickwin.com>
2025-12-31 17:03:41 +0530

    const.ts updated

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,11 @@
-export const VITE_API_BASE_URL = "http://127.0.0.1:8000/api"
-export const VITE_API_ROOT_URL = "http://127.0.0.1:8000"
+// export const VITE_API_BASE_URL = "http://127.0.0.1:8000/api"
+// export const VITE_API_ROOT_URL = "http://127.0.0.1:8000"
+
+
+export const VITE_API_BASE_URL = "https://ecwriterapi.educatedapp.com/api"
+export const VITE_API_ROOT_URL = "https://ecwriterapi.educatedapp.com"
+
+
+export const VITE_SUPABASE_PROJECT_ID="jybgtlnprbyjumtkhjww"
+export const VITE_SUPABASE_PUBLISHABLE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5Ymd0bG5wcmJ5anVtdGtoand3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyODMzNzMsImV4cCI6MjA4MDg1OTM3M30.xFYF8VanycNOc_vX4Wbjc85Kwt7i9Z5OMMBJy60wbFc"
+export const VITE_SUPABASE_URL="https://jybgtlnprbyjumtkhjww.supabase.co"
